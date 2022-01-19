d3bc75bd1bb8f03501e8d6931ca28b75857c450a
scottmacleod <t_mac3131@hotmail.com>
2022-01-19 10:49:15 -0600

    added modal and got edit working

--- src/components/Heroes.jsx
+++ src/components/Heroes.jsx
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Modal from './Modal';
 
 function Heroes(props) {
     const [heroes, setHeroes] = useState([])
+    const [modalInfo, setModalInfo] = useState("")
+    const [show, setShow] = useState(false)
+
     const superPersonInfo = {
-      aliases: 'none listed',
-      alter_ego: 'none listed',
-      career: 'none listed',
-      comics: 'none listed',
-      creators: 'no creators listed',
-      description: 'none listed',
-      image_url: 'none listed',
-      movies: 'none listed',
-      name: 'The Flash',
-      origin_story: 'none listed',
-      other_forms_of_media: 'none listed',
-      place_of_residence: 'none listed',
-      powers: 'none listed'
+      // aliases: 'none listed',
+      // alter_ego: 'Barry Allen',
+      // career: 'none listed',
+      // comics: 'none listed',
+      // creators: 'no creators listed',
+      // description: 'none listed',
+      image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjspaUfQA9lrBMCWrGSQDiLnGT9hvvFyTMIA&usqp=CAU'
+      // movies: 'none listed',
+      // name: 'The Flash',
+      // origin_story: 'none listed',
+      // other_forms_of_media: 'none listed',
+      // place_of_residence: 'none listed',
+      // powers: 'none listed'
     }
-    const [modalInfo, setModalInfo] = useState(superPersonInfo)
+
     function getData() {
-        axios.get("http://localhost:8000/superheroes/").then((res) => {
+        axios.get("https://rocky-waters-42590.herokuapp.com/superheroes/").then((res) => {
           const superHeroData = res.data;
           setHeroes(superHeroData)
           console.log(superHeroData)
@@ -29,7 +33,7 @@ function Heroes(props) {
 
       function postHero(event) {
         // event.preventDefault();
-        axios.post("http://localhost:8000/superheroes/", modalInfo).then((res) => {
+        axios.post("https://rocky-waters-42590.herokuapp.com/superheroes/", modalInfo).then((res) => {
           const superHeroData = res.data;
           console.log("it's getting here")
           setHeroes(heroes => [...heroes, superHeroData]);
@@ -38,14 +42,32 @@ function Heroes(props) {
         })
       }
 
+    //   function editHeroInfo(event, id) {
+    //     axios.put(`https://rocky-waters-42590.herokuapp.com/superheroes/1`, superPersonInfo).then((res) => {
+    //       const superHeroData = res.data;
+    //       console.log("it's getting here")
+    //       setHeroes(heroes => [...heroes, superHeroData]);
+    //       console.log(heroes)
+    //     })
+    // }
+
+    function handleShowItem(data) {
+      setModalInfo(data);
+    }
+
+    function onClick(item) {
+      setShow(true);
+      handleShowItem(item);
+    }
+
     return (
         <div>
             This is Heroes
-            <button onClick={postHero}>Add Hero</button>
+            {/* <button onClick={editHeroInfo}>edit Flash</button> */}
             <button onClick={getData}>Heroes</button>
             {heroes.map(hero => {
               return (
-                <div>
+                <div onClick={() => onClick(hero)} key={hero.id}>
                   <div>
                     <h1>{hero.name}</h1>
                   </div>
@@ -58,6 +80,14 @@ function Heroes(props) {
                 </div>
               )
             })}
+            <Modal
+              onClose={() => setShow(false)} 
+              show={show}
+              modalInfo={modalInfo}
+              setModalInfo={setModalInfo}
+              heroes={heroes}
+              setHeroes={setHeroes}
+            />
         </div>
     );
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,18 +6,96 @@ function Modal(props) {
         return null;
 
     function editSuperHeroInfo(event, id) {
-        event.preventDefault();
-        axios.put(`http://localhost:8000/superheroes/${id}`, props.modalInfo).then((res) => {
+        // event.preventDefault();
+        axios.put(`https://rocky-waters-42590.herokuapp.com/superheroes/${id}`, props.modalInfo).then((res) => {
             const superHeroData = res.data;
-            props.setSuperHeroInfo({...props.superHeroInfo, superHeroData});
-            // event.preventDefault();
+            props.setHeroes({...props.superHeroInfo, superHeroData});
+            event.preventDefault();
+        })
+    }
+
+    function deleteHero(event, id) {
+        axios.delete(`https://rocky-waters-42590.herokuapp.com/superheroes/${id}`).then((res) => {
+            const bookData = res.data;
+            props.setHeroes(bookData);
+            event.preventDefault();
         })
     }
     
     return (
-        <div>
-            
+        <div className='modal'>
+            <div className='modal-content'>
+                <div className='modal-header'>
+                    <h4 className='modal-title'>{props.modalInfo.title}</h4>
+                </div>
+                <div className='modal-body'>
+                    {props.modalInfo.description}
+                    <div className='location-div'>
+                        Location: {props.modalInfo.location}
+                    </div>
+                </div>
+                <div className='modal-footer'>
+                    <form>
+                        <div>
+                            <label id="edit-name-label">Edit Name:</label>
+                            <input type="text" id="edit-name-input" onChange={(event) => props.setModalInfo({...props.modalInfo, name: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <label id="edit-creator-label">Edit Creators:</label>
+                            <input placeholder={props.modalInfo.creators} type="text" id="edit-creator-input" onChange={(event) => props.setModalInfo({...props.modalInfo, creators: event.target.value})}></input>
+                        </div>
+                        {/* <div>
+                            <label id="edit-genre-label">Edit Genre:</label>
+                            <input placeholder={props.modalInfo.genre} type="text" id="edit-genre-input" onChange={(event) => props.setModalInfo({...props.modalInfo, genre: event.target.value})}></input>
+                        </div> */}
+                        {/* <div>
+                            <label id="edit-published-label">Edit Year Published:</label>
+                            <input placeholder={props.modalInfo.published} type="text" id="edit-published-input" onChange={(event) => props.setModalInfo({...props.modalInfo, published: event.target.value})}></input>
+                        </div> */}
+                        {/* <div>
+                            <label id="edit-isbn-label">Edit ISBN:</label>
+                            <input placeholder={props.modalInfo.isbn} type="text" id="edit-isbn-input" onChange={(event) => props.setModalInfo({...props.modalInfo, isbn: event.target.value})}></input>
+                        </div> */}
+                        {/* <div>
+                            <label id="edit-format-label">Edit Format:</label>
+                            <input placeholder={props.modalInfo.format} type="text" id="edit-format-input" onChange={(event) => props.setModalInfo({...props.modalInfo, format: event.target.value})}></input>
+                        </div> */}
+                        {/* <div>
+                            <label id="edit-description-label">Edit Description:</label>
+                            <textarea rows="5" columns="1" id="edit-description-input" onChange={(event) => props.setModalInfo({...props.modalInfo, description: event.target.value})}></textarea>
+                        </div>
+                        <div>
+                            <label id="edit-location-label">Edit Location:</label>
+                            <input type="text" id="edit-location-input" onChange={(event) => props.setModalInfo({...props.modalInfo, location: event.target.value})}></input>
+                        </div> */}
+                        {/* <div>
+                            <label id="edit-image-label">Edit Image URL:</label>
+                            <input placeholder={props.modalInfo.image} type="text" id="edit-image-input" onChange={(event) => props.setModalInfo({...props.modalInfo, image: event.target.value})}></input>
+                        </div>
+                        <div>
+                            <input type="checkbox" onChange={(event) => props.setModalInfo({...props.modalInfo, available: !event.target.checked})} checked={!props.modalInfo.available}></input>
+                            <label>
+                                Checked Out
+                            </label>
+                        </div> */}
+                        <button 
+                            className='edit-info-button' 
+                            type="submit" 
+                            onClick={(event) => editSuperHeroInfo(event, props.modalInfo.id)} 
+                        >
+                            Edit
+                        </button>
+                        <button 
+                            onClick={(event) => deleteHero(event, props.modalInfo.id)}
+                        >
+                            Delete Hero
+                        </button>
+                    </form>
+                    <button className='modal-button' onClick={props.onClose}>Close</button>
+                </div>
+            </div>
         </div>
+    
     );
 }
 
