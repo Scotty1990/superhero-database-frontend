44412d0ca7a5986d2e32a406cfc0310a62766563
scottmacleod <t_mac3131@hotmail.com>
2022-01-21 12:47:47 -0600

    added home link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,7 @@ function Nav(props) {
     return (
         <div>
             <Link id='heroes-link' to="/heroes">Heroes</Link>
+            <Link id='home-link' to='/'>Home</Link>
             <Link id='villains-link' to="/villains">Villains</Link>
         </div>
     );
